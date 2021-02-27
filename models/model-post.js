148a0165ed15b09c2039bb4290ca6ddd7b0178cb
Kexin Zheng <kexinz0227@g.ucla.edu>
2021-02-27 13:35:34 -0800

    created basic frame for handling users, posts, comments, and courses (for categorizing posts) Unfinished

---
+++ models/model-post.js
@@ -0,0 +1,47 @@
+// database setup
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+const server = '127.0.0.1:27017';
+const database = 'db97';
+const user = 'cs97user';
+const password = 'cs97pwd';
+
+// login to database
+mongoose.connect(`mongodb://${user}:${password}@${server}/${database}`, { useNewUrlParser: true, 'useUnifiedTopology': true, 'useCreateIndex': true}, (err) =>{
+    throw err;
+});
+
+// User
+let PostSchema = new Schema({
+    title: {
+        type: String, 
+        required: true,
+    }, 
+    user: {
+        type: Schema.Types.ObjectId, 
+        ref: "User", 
+    },
+    dateupdate:{
+        type: Date, 
+    }, 
+    content: {
+        type: String, 
+        required: true,
+    },
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: "Course",
+        required: true,
+    }, 
+    upvote: [{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    comment: [{
+        type: Schema.Types.ObjectId, 
+        ref: "Comment",
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', PostSchema);
