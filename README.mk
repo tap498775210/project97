ab480bba9bde0db13398e966c2271083f3a9290d
AnPing <anpgtao@gmail.com>
2021-03-11 21:06:06 -0800

    added the readme to the repo

---
+++ README.mk
@@ -0,0 +1,21 @@
+#Qiazza!!!
+---
+This is our project for our CS97 2021 winter
+This porject behave like piazza that people can post questions and answer questions
+after they log into the website
+---
+To make this project run, you need to do
+1. make sure node and npm is installed and is running on your device
+2. open two CLIs under current directory which contains this file
+3. set one of the CLI's working directory to api/ by running
+	`cd api`
+4. set another CLI's working directory to client/ by running
+	`cd client`
+5. In both CLIs install the dependencies by running
+	`npm install`
+6. In the CLI whose working directory is api/ running
+	`npm start`
+7. In the CLI whose working directory is client/ running
+	`npm start`
+---
+Then the project should run appropriately.
